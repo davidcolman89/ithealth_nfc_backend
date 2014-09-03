f83c632666eac0e919331cb17e3f1c7a2e7e4071
@_davidcolman <colmanrodrigo@gmail.com>
2014-09-03 17:50:34 -0300

    Update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'ithealth_nfc',
+			'database'  => '',
 			'username'  => 'root',
-			'password'  => '3LB0CH4',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
